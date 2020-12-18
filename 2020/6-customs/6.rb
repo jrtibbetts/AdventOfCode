393a6460537dacda611ab61ba613c470d7bcde4d
Jason R Tibbetts <jason@tibbetts.net>
2020-12-18 18:31:05 -0500

    Finished Day 6, task 2.

--- 2020/6-customs/6.rb
+++ 2020/6-customs/6.rb
@@ -1,12 +1,29 @@
 #!/usr/bin/env ruby
+require 'Set'
 
-total = 0
+total_unique_answers = 0
+total_shared_answers = 0
 
 IO.read("6.input").split("\n\n").each_with_index { | line, index |
     unique_values = line.split("\n").join.chars.uniq.sort
-    sum = unique_values.count
-    total += sum
-    p index.to_s + ": Unique characters in line " + line + ": " + unique_values.join(",") + " (" + sum.to_s + ")"
+    total_unique_answers += unique_values.count
+    p index.to_s + ": Unique characters in line " + line + ": " + unique_values.join(",") + " (" + unique_values.count.to_s + ")"
+
+    shared_values = Set[]
+
+    line.split("\n").each_with_index { | line, index |
+        charset = Set.new(line.chars)
+
+        if index == 0
+            shared_values = charset
+        else
+            shared_values &= charset
+        end
+    }
+
+    p "Shared characters:" + shared_values.count.to_s
+    total_shared_answers += shared_values.count
 }
 
-p "total: " + total.to_s
+p "Unique answers total: " + total_unique_answers.to_s
+p "Shared answers total: " + total_shared_answers.to_s
