f683a0e5e0eabf7a84cfdfd9e9622b012c0b7dd3
Jason R Tibbetts <jason@tibbetts.net>
2020-12-17 23:25:35 -0500

    I got 159, but the correct answer is 158. Why am I off by 1?

--- 2020/4-passports/4.rb
+++ 2020/4-passports/4.rb
@@ -13,36 +13,39 @@ def part_2_valid?(passport)
         (field, value) = entry.split(':')
         case field
         when /byr/
-            if 1920 <= value.to_i && value.to_i <= 2002
+            if !(1920..2002).include? value.to_i
+                p "Wrong birth year: " + value
                 return false
             end 
         when /iyr/
-            if 2010 <= value.to_i && value.to_i <= 2020
+            if !(2010..2020).include? value.to_i
+                p "Wrong issue year: " + value
                 return false
             end
         when /eyr/
-            if 2020 <= value.to_i && value.to_i <= 2030
+            if !(2020..2030).include? value.to_i
+                p "Wrong expiration year: " + value
                 return false
             end
         when /hgt/
-            matches = /(\d*)([in|cm])/.match(value)
+            matches = /(\d*)(in|cm)/.match(value)
 
             if matches == nil
-                p "Height is in the wrong format"
+                p "Wrong height format: " + value
                 return false
             end
 
-            measurement = $1.to_i
-            units = $2.to_s
-
-            p "Height: " + measurement.to_s + units
+            measurement = matches[1].to_i
+            units =  matches[2].to_s
 
             if units == 'cm'
-                if measurement < 150 || measurement > 193
+                if !(150..193).include? measurement
+                    p "Wrong centimeters value: " + measurement.to_s
                     return false
                 end
             else
-                if measurement < 59 || measurement > 76
+                if !(59..76).include? measurement
+                    p "Wrong inches value: " + measurement.to_s
                     return false
                 end
             end
@@ -52,17 +55,19 @@ def part_2_valid?(passport)
                 return false
             end
         when /ecl/
-            if !"amb blu brn gry grn hzl oth".split.include?(value)
+            if !("amb blu brn gry grn hzl oth".split.include? value)
                 p "Wrong eye color: " + value
+                return false
             end
         when /pid/
             if !/[0-9]{9}/.match?(value)
                 p "Wrong passport number: " + value
+                return false
             end
         end
     }
 
-    true
+    return true
 end
 
 IO.read("4.input").split("\n\n").each { | line |
