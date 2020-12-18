1a2ad5972361bf3b5081870d37df1f264b3a0e62
Jason R Tibbetts <jason@tibbetts.net>
2020-12-18 17:53:02 -0500

    Finished Day 6, part 1.

--- 2020/6-customs/6.rb
+++ 2020/6-customs/6.rb
@@ -3,11 +3,10 @@
 total = 0
 
 IO.read("6.input").split("\n\n").each_with_index { | line, index |
-    unique_values = line.chars.uniq
-    sum = unique_values.count - 1 # drop the newline
+    unique_values = line.split("\n").join.chars.uniq.sort
+    sum = unique_values.count
     total += sum
-    p index.to_s + ": Unique characters in line " + line + ": " + unique_values.sort.join(",") + " (" + sum.to_s + ")"
+    p index.to_s + ": Unique characters in line " + line + ": " + unique_values.join(",") + " (" + sum.to_s + ")"
 }
-p IO.read("6.input").split("\n\n").count.to_s
 
 p "total: " + total.to_s
