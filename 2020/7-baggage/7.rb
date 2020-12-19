7f339e7e81eb8a1e7530d541fc7af4ea9f3dcec5
Jason R Tibbetts <jason@tibbetts.net>
2020-12-19 02:10:36 -0500

    Added Day 7, task 1.

--- 2020/7-baggage/7.rb
+++ 2020/7-baggage/7.rb
@@ -2,15 +2,13 @@
 
 require 'Set'
 
-@bags = { }
-
-def bags_contained_by_color(color)
-    contained_bags = @bags[color]
+def bags_contained_by_color(color, bags)
+    contained_bags = bags[color]
 
     if contained_bags == nil
         p "Creating a bag for " + color
-        contained_bags = Set[]
-        @bags[color] = contained_bags
+        contained_bags = Set[color]
+        bags[color] = contained_bags
     elsif !contained_bags.empty?
         p "Found an existing bag: " + contained_bags.to_s
     end
@@ -18,19 +16,23 @@ def bags_contained_by_color(color)
     return contained_bags
 end
 
+bags_by_color = { }
+
 IO.readlines("7.input").each { | line |
     p line
-    /(.*) bags contain (.*)./.match(line)
+    /(.*) bags contain (.*)./ =~ line
     
     bag_color = $1
-    bags = bags_contained_by_color($1)
+    sub_bags = bags_contained_by_color($1, bags_by_color)
 
     $2.split(', ').each { | color_string |
-        /\d* (.*) bag.*/.match(color_string)
+        /\d* (.*) bag.*/ =~ color_string
         p "Adding '" + $1 + "' to bag '" + bag_color + "'"
-        bags < bags_contained_by_color($1)
+        sub_bag = bags_contained_by_color($1, bags_by_color)
+        sub_bags << sub_bag
     }
 
-    @bags[bag_color] = bags
-    p
+    p "Adding " + sub_bags.count.to_s + " bags to " + bag_color
+    bags_by_color[bag_color] = sub_bags
+    p "\n"
 }
