5841e650923e4ad5ac94a1974e6ad97b6a4ebbd2
Jason R Tibbetts <jason@tibbetts.net>
2020-12-20 19:48:25 -0500

    Day 7, part 2. Still doesn't get the correct answer.

--- 2020/7-baggage/7.rb
+++ 2020/7-baggage/7.rb
@@ -6,12 +6,9 @@ def bags_contained_by_color(color, bags)
     contained_bags = bags[color]
 
     if contained_bags == nil
-        p "Creating a bag for " + color
-        contained_bags = Set[color]
+        contained_bags = [color] # Set[color]
         bags[color] = contained_bags
-    elsif !contained_bags.empty?
-        p "Found an existing bag: " + contained_bags.to_s
-    end
+     end
 
     return contained_bags
 end
@@ -19,20 +16,28 @@ end
 bags_by_color = { }
 
 IO.readlines("7.input").each { | line |
-    p line
     /(.*) bags contain (.*)./ =~ line
     
     bag_color = $1
     sub_bags = bags_contained_by_color($1, bags_by_color)
 
     $2.split(', ').each { | color_string |
-        /\d* (.*) bag.*/ =~ color_string
-        p "Adding '" + $1 + "' to bag '" + bag_color + "'"
-        sub_bag = bags_contained_by_color($1, bags_by_color)
-        sub_bags << sub_bag
-    }
+        /\d+ (.*) bag.*/ =~ color_string
 
-    p "Adding " + sub_bags.count.to_s + " bags to " + bag_color
-    bags_by_color[bag_color] = sub_bags
-    p "\n"
+        if $& != nil
+            sub_bag = bags_contained_by_color($1, bags_by_color)
+            sub_bags << sub_bag
+        end
+    }
 }
+
+gold_container_count = 0
+
+bags_by_color.each_value { | sub_bag |
+    if sub_bag.flatten.include? "shiny gold"
+        gold_container_count += 1
+    end
+}
+
+p gold_container_count.to_s + " bags can contain shiny gold."
+
