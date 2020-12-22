1e89f8b09fe597757eceb2eb971096d57d311e79
Jason R Tibbetts <jason@tibbetts.net>
2020-12-21 23:37:17 -0500

    Day 7, task 2. Still not the correct answer.

--- 2020/7-baggage/7.rb
+++ 2020/7-baggage/7.rb
@@ -13,19 +13,36 @@ def bags_contained_by_color(color, bags)
     return contained_bags
 end
 
-bags_by_color = { }
+def number_of_bags_contained_by(bag_color)
+    p "Finding number of bags contained by " + bag_color + ": "
+    sub_bags = @bags[bag_color]
+
+    if sub_bags == nil
+        p "none"
+        return 0
+    else
+        p sub_bags.count.to_s
+        # p "Sub bags: " + sub_bags.to_s # .to_a.join(", ")
+        return sub_bags.count + sub_bags.inject(0) { | sum, sub_bag | sum + number_of_bags_contained_by(sub_bag)}
+    end
+end
+
+@bags_by_color = { }
+@bags = { }
 
 IO.readlines("7.input").each { | line |
     /(.*) bags contain (.*)./ =~ line
     
     bag_color = $1
-    sub_bags = bags_contained_by_color($1, bags_by_color)
+    sub_bags = bags_contained_by_color($1, @bags_by_color)
+    @bags[bag_color] = []
 
     $2.split(', ').each { | color_string |
         /\d+ (.*) bag.*/ =~ color_string
-
         if $& != nil
-            sub_bag = bags_contained_by_color($1, bags_by_color)
+            @bags[bag_color] << $1
+
+            sub_bag = bags_contained_by_color($1, @bags_by_color)
             sub_bags << sub_bag
         end
     }
@@ -33,11 +50,11 @@ IO.readlines("7.input").each { | line |
 
 gold_container_count = 0
 
-bags_by_color.each_value { | sub_bag |
+@bags_by_color.each_value { | sub_bag |
     if sub_bag.flatten.include? "shiny gold"
         gold_container_count += 1
     end
 }
 
 p gold_container_count.to_s + " bags can contain shiny gold."
-
+p "A shiny gold bag contains " + number_of_bags_contained_by("shiny gold").to_s
