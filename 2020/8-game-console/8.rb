9acd19a22220b841eef6c14b7abe7f418e2cfbdc
Jason R Tibbetts <jason@tibbetts.net>
2020-12-25 02:28:02 -0500

    Day 8, task 2 finished.

--- 2020/8-game-console/8.rb
+++ 2020/8-game-console/8.rb
@@ -31,33 +31,41 @@ end
 
 def can_execute_task_2
     if @executed_lines.include? @instruction_index
-        instruction = @program[@instruction_index]
+        instruction = @input_
+    program[@instruction_index]
         p "Duplicate instruction: " + instruction
 
         case instruction
         when /jmp/
-            @program[@instruction_index] = instruction.gsub(/jmp/, "nop")
+            @input_
+        program[@instruction_index] = instruction.gsub(/jmp/, "nop")
         when /nop/
-            @program[@instruction_index] = instruction.gsub(/nop/, "jmp")
+            @input_
+        program[@instruction_index] = instruction.gsub(/nop/, "jmp")
         end
 
-        p "Replaced with: " + @program[@instruction_index]
+        p "Replaced with: " + @input_
+    program[@instruction_index]
     end
 
     true
 end
 
-def run_program(can_execute)
-    while self.send(can_execute)
-        if @instruction_index >= @program.count
+def run(program)
+    @accumulator = 0
+    @instruction_index = 0
+    @executed_lines = Set[]
+
+    while !@executed_lines.include? @instruction_index
+        if @instruction_index >= program.count
             p "Program terminated successfully! Accumulator is " + @accumulator.to_s
-            break
+            return true
         end
 
         # p @instruction_index.to_s + ": " + @program[@instruction_index]
         @executed_lines << @instruction_index
 
-        case @program[@instruction_index]
+        case program[@instruction_index]
         when /acc ([+-]\d+)/
             accumulate($1.to_i)
         when /jmp ([+-]\d+)/
@@ -68,10 +76,40 @@ def run_program(can_execute)
     end
 
     p "Accumulator is " + @accumulator.to_s + " before looping."
+    return false
 end
 
 # Task 1
-run_program(:can_execute_task_1)
+run(@program)
 
 # Task 2
-run_program(:can_execute_task_2)
+p "Task 2"
+
+jmp_and_nop_indices = []
+
+@program.each_with_index { | line, index |
+    if line =~ /jmp/ || line =~ /nop/
+        jmp_and_nop_indices << index
+    end
+}
+
+# p "Indices: " + jmp_and_nop_indices.join(", ")
+while !jmp_and_nop_indices.empty?
+    index = jmp_and_nop_indices.shift
+    program_clone = @program.clone(freeze: false)
+
+    line = program_clone[index]
+
+    case line
+    when /jmp/
+        program_clone[index] = line.gsub(/jmp/, "nop")
+    when /nop/
+        program_clone[index] = line.gsub(/nop/, "jmp")
+    end
+
+    p "Replacing " + line + " with " + program_clone[index]
+    if run(program_clone)
+        p "Final accumulator value: " + @accumulator.to_s
+        break
+    end
+end
