150c11e294ffc3a1a062f1955c134344aef1e6b6
Jason R Tibbetts <jason@tibbetts.net>
2020-12-24 21:30:48 -0500

    Starting Day 8, task 2. I don't know how I'm going to do this.

--- 2020/8-game-console/8.rb
+++ 2020/8-game-console/8.rb
@@ -2,28 +2,48 @@
 
 require 'Set'
 
-executed_lines = Set[]
-accumulator = 0
-instruction_index = 0
+@executed_lines = Set[]
+@accumulator = 0
+@instruction_index = 0
 
-program = IO.readlines("8.input")
+@program = IO.readlines("8.input")
+@program.freeze
 
-while !executed_lines.include? instruction_index
-    p instruction_index.to_s + ": " + program[instruction_index]
-    executed_lines << instruction_index
+def run_program
+    while !@executed_lines.include? @instruction_index
+        if @instruction_index >= @program.count
+            p "Program terminated successfully! Accumulator is " + @accumulator.to_s
+            break
+        end
 
-    case program[instruction_index]
-    when /acc ([+-]\d+)/
-        p "Adding " + $1
-        accumulator += $1.to_i
-        instruction_index += 1
-    when /jmp ([+-]\d+)/
-        instruction_index += $1.to_i
-        p "Jumping to " + instruction_index.to_s
-    when /nop/
-        p "Doing nothing"
-        instruction_index += 1
+        p @instruction_index.to_s + ": " + @program[@instruction_index]
+        @executed_lines << @instruction_index
+
+        case @program[@instruction_index]
+        when /acc ([+-]\d+)/
+            # p "Adding " + $1
+            @accumulator += $1.to_i
+            @instruction_index += 1
+        when /jmp ([+-]\d+)/
+            @instruction_index += $1.to_i
+            # p "Jumping to " + @instruction_index.to_s
+        when /nop/
+            # p "Doing nothing"
+            @instruction_index += 1
+        end
     end
+
+    p "Accumulator is " + @accumulator.to_s + " before looping."
 end
 
-p "Accumulator is " + accumulator.to_s + " before looping."
+# Task 1
+run_program
+
+# Task 2
+jmp_nop_indices = @program.each_with_index.filter_map { | line, index |
+    if /jmp/ =~ line || /nop/ =~ line
+        return 
+    end
+}
+
+
