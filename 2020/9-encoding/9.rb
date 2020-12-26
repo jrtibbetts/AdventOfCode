ea2b467bc2ad3e849d54c50988c5afce3fd8421a
Jason R Tibbetts <jason@tibbetts.net>
2020-12-26 02:10:09 -0500

    Day 9, task 2: Solved.

--- 2020/9-encoding/9.rb
+++ 2020/9-encoding/9.rb
@@ -20,14 +20,44 @@ def window_contains(window, value)
     return false
 end
 
+@target_number
+
 while index < input.count
     number = input[index]
     preceding_values = input[(index - window_count)...index]
 
     if !window_contains(preceding_values, number)
+        @target_number = number
         p number.to_s + " isn't the sum of any two values in " + preceding_values.to_s
         break
     end
 
     index += 1
 end
+
+input.each_with_index { | number, index |
+    if input[index] != @target_number
+        # p "Starting a run with " + index.to_s + " (" + input[index].to_s + ")"
+        run = []
+        sum = 0
+
+        input[index...input.length].each_with_index { | j, j_index |
+            new_sum = sum + j
+
+            if new_sum == @target_number
+                run << j_index + index
+                p run.to_s
+                p "The sequence that adds up to " + @target_number.to_s + " is " + run.map { | x | input[x] }.to_s
+                sorted_values = run.map { | x | input[x] }.sort
+                p "The smallest and largest number in this sequence add up to " + (sorted_values.first + sorted_values.last).to_s
+                return
+            elsif new_sum < @target_number
+                # p "    Appending " + j.to_s
+                run << j_index + index
+                sum = new_sum
+            else
+                break
+            end
+        }
+    end
+}
