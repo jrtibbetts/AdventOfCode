9067b4fd4b7257eece43fa913e23922a346f7902
Jason R Tibbetts <jason@tibbetts.net>
2021-12-01 07:12:50 -0500

    No ticket: [add] Added task 2 for puzzle 10.

--- 2020/10-adapters/10.rb
+++ 2020/10-adapters/10.rb
@@ -18,3 +18,20 @@ input.each_with_index { | jolts, index |
 }
 
 p "Single-jolt differences times triple-jolt differences: " + (single_jolt_differences.count * triple_jolt_differences.count).to_s
+
+# Task 2: Find all possible permutations
+
+def all_permutations(array)
+    p "All permutations for array " + array.length.to_s 
+    
+    if array.length < 1
+        return array.length
+    else
+        first_value = array.shift
+        next_values = array.drop(1).select { | x | (x - first_value) <= 3 }
+        
+        return next_values.inject(next_values.length) { | sum, values | sum + all_permutations(values) }
+    end
+end
+
+p "All possible permutations: " + all_permutations(input)
