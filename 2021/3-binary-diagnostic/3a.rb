813a94cecc9007fbf511c72851e23101f39b2f7c
Jason R Tibbetts <jason@tibbetts.net>
2021-12-04 00:40:40 -0500

    Cleanup.

--- 2021/3-binary-diagnostic/3a.rb
+++ 2021/3-binary-diagnostic/3a.rb
@@ -1,7 +1,6 @@
 #!/usr/bin/env ruby
 
 input_lines = IO.readlines("3.input").map { | line | line.strip }.select { | line | !line.empty?}
-p input_lines[0..100]
 line_count = input_lines.count
 first_line = input_lines[0]
 
@@ -10,18 +9,15 @@ epsilon_chars = Array.new(first_line.length)
 
 first_line.length.times { | index |
     ones_at_index = input_lines.map { | s | s[index].to_i }.reduce(:+)
+    more_ones_than_zeroes = ones_at_index > (line_count / 2)
 
-    if ones_at_index > (line_count / 2)
-        gamma_chars[index] = 1
-        epsilon_chars[index] = 0
-    else
-        gamma_chars[index] = 0
-        epsilon_chars[index] = 1
-    end
-
+    gamma_chars[index] = (more_ones_than_zeroes ? 1 : 0)
+    epsilon_chars[index] = (more_ones_than_zeroes ? 0 : 1)
+ 
     p "Index ", index, " ones: ", ones_at_index
 }
 
+# convert chars into a string, then convert the binary value into a decimal
 gamma_level = gamma_chars.join.to_i(2)
 epsilon_level = epsilon_chars.join.to_i(2)
 p "Gamma chars: ", gamma_level
