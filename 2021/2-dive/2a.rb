fa7edd4065781c942873f864cb27ce7f46f6f144
Jason R Tibbetts <jason@tibbetts.net>
2021-12-03 21:38:55 -0500

    Day 2, task 2. Just added the second task's code into the first task.

--- 2021/2-dive/2a.rb
+++ 2021/2-dive/2a.rb
@@ -3,19 +3,27 @@
 input_lines = IO.readlines("2.input")
 horizontal_position = 0
 depth = 0
+aim = 0
+depth_with_aim = 0
 
 input_lines.each { | line |
     (direction, units) = line.split
+    unitsInt = units.to_i
 
     if direction == "forward"
-        horizontal_position += units.to_i 
+        horizontal_position += unitsInt
+        depth_with_aim += unitsInt * aim
     elsif direction == "down"
-        depth += units.to_i
+        depth += unitsInt
+        aim += unitsInt
     else
-        depth -= units.to_i
+        depth -= unitsInt
+        aim -= unitsInt
     end
 }
 
 p "Horizontal position: " + horizontal_position.to_s
-p "Depth: " + depth.to_s
+p "Depth: ", depth
+p "Depth with aim: " + depth_with_aim.to_s
 p "Multiplied: " + (depth * horizontal_position).to_s
+p "Multiplied with aim: " + (depth_with_aim * horizontal_position).to_s
