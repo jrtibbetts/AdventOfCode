5a9853aa140473632eafe5ecd3354029c554f55f
Jason R Tibbetts <jason@tibbetts.net>
2021-12-06 00:10:04 -0500

    Day 5, task 1. TOO LOW.

--- 2021/5-hydrothermal-venture/5a.rb
+++ 2021/5-hydrothermal-venture/5a.rb
@@ -14,12 +14,20 @@ class Line
         @start.join(",") + " -> " + @end.join(",")
     end
 
+    def is_horizontal?
+        return @start[0] == @end[0]
+    end
+
+    def is_vertical?
+        @start[1] == @end[1]
+    end
+
     def is_horizontal_or_vertical?
-        return @start[0] == @end[0] || @start[1] == @end[1]
+        return is_horizontal? || is_vertical?
     end
 
     def horizontal_delta
-        diff = @start[0] - @end[0]
+        diff = @start[1] - @end[1]
         
         if diff == 0
             return 0
@@ -31,25 +39,31 @@ class Line
     end
 
     def vertical_delta
-        diff = @start[1] - @end[1]
+        diff = @start[0] - @end[0]
 
         if diff == 0
             return 0
         elsif diff < 1
             return 1
         else
-            return 1
+            return -1
         end
     end
    
     def points
-        p "Start: " + @start.to_s
-        p "End: " + @end.to_s
         points = Array.new
-        diff = (@start[0] - @end[0])
-        
-        diff.times { | index |
-            new_point = [(@start[0] + horizontal_delta), (@start[1] + vertical_delta)]
+        diff = [(@start[0] - @end[0]).abs, (@start[1] - @end[1]).abs].max
+        new_x = @start[0]
+        new_y = @start[1]
+
+        diff.times { | index |    
+            if is_horizontal?
+                new_x += horizontal_delta
+            elsif is_vertical?
+                new_y += vertical_delta
+            end
+
+            new_point = [new_x, new_y]
             points.push new_point
         }
 
@@ -59,13 +73,21 @@ end
 
 input_lines = IO.readlines("5.input").map { | line | line.strip }.select { | line | !line.empty?}
 lines = Array.new
+grid = Hash.new
+
 input_lines.each { | line_string |
     line = Line.new line_string
     lines.push line
     
     if line.is_horizontal_or_vertical?
-        p "Points: " + line.points.to_s
+        line.points.each { | point |
+            if !(grid.include? point)
+                grid[point] = 1
+            else
+                grid[point] += 1
+            end
+        }
     end
-
-    # p "Line " + line.to_s + "; vertical or horizontal? " + line.is_horizontal_or_vertical?.to_s
 }
+
+p grid.select { | key, value | value > 1 }.count
