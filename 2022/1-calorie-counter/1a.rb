5c60f452164336f432c9d865b9e51cbce778ec86
Jason R Tibbetts <jason@tibbetts.net>
2022-12-01 13:34:57 -0500

    Day 1, part 2 complete.

--- 2022/1-calorie-counter/1a.rb
+++ 2022/1-calorie-counter/1a.rb
@@ -1,23 +1,34 @@
 #!/usr/bin/env ruby
 
-elves = Array.new
+    elves = Array.new
 
-current_elf_calorie_count = 0
+    current_elf_calorie_count = 0
 
-input_lines = IO.readlines("1.input")
+    input_lines = IO.readlines("1.input")
 
-input_lines.each { | line |
-    p "Current calorie count: " + current_elf_calorie_count.to_s
+    input_lines.each { | line |
+        stripped_line = line.strip
 
-    if line.strip.empty?
-        if current_elf_calorie_count > 0
-            elves.push current_elf_calorie_count
+        if stripped_line.empty?
+            if current_elf_calorie_count > 0
+                elves.push current_elf_calorie_count
+            end
+
+            current_elf_calorie_count = 0
+        else
+            current_elf_calorie_count += stripped_line.to_i 
         end
+    }
 
-        current_elf_calorie_count = 0
-    else
-        current_elf_calorie_count += line.strip.to_i 
+    if current_elf_calorie_count > 0
+        elves.push current_elf_calorie_count
     end
-}
 
-p elves.max
+    puts "All calorie counts, sorted: " + elves.to_s
+    puts "Highest calorie count: " + elves.max.to_s
+
+    sorted_calorie_counts = elves.sort.reverse
+    puts "All calorie counts, sorted: " + sorted_calorie_counts.to_s
+    three_highest_counts = sorted_calorie_counts[0 ... 3]
+    puts "Three highest calorie counts: " + three_highest_counts.to_s
+    puts "Highest three calorie count total: " + three_highest_counts.sum.to_s
